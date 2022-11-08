4c983d60078a0299af24db8b1e52bbd0b3c4bbb6
Suembike <Simochka.violet@mail.ru>
2022-11-08 18:41:48 +0300

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,15 @@ const path = {
 		dir: 'src/index.html',
 		templates: 'src/**/*.html',
 		script: 'src/scripts/*.js',
-		style: 'src/styles/*.css',
-		images: 'src/imgs/*',
+		style: 'src/styles/**/*.pcss',
+		images: 'src/images/*',
 		fonts: 'src/fonts/*'
 	},
 	buildFolder: {
 		dir: 'build',
 		script: 'build/js',
 		style: 'build/css',
-		images: 'build/imgs',
+		images: 'build/images',
 		fonts: 'build/fonts'
 	},
 	buildName: {
@@ -40,7 +40,7 @@ gulp.task('buildCss', () => {
 		],
 	processors = [			
 		assets({
-			loadPaths: ['../images', 'src/images']
+			loadPaths: ['../images/', 'src/images/']
 		})]
 	return gulp.src([path.src.style])
 		.pipe(postcss(processors))
@@ -86,6 +86,11 @@ gulp.task('buildJs-watch', gulp.series('buildJs', (done) => {
 	done();
 }));
 
+gulp.task('buildImgs-watch', gulp.series('buildImgs', (done) => {
+	browserSync.reload();
+	done();
+}));
+
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
@@ -93,11 +98,12 @@ gulp.task('browser-sync', () => {
         }
     });
 	gulp.watch(path.src.style, gulp.series('buildCss-watch'));
+	gulp.watch(path.src.images, gulp.series('buildImgs-watch'));
 	gulp.watch(path.src.templates, gulp.series('buildHtml-watch'));
 	gulp.watch(path.src.script, gulp.series('buildJs-watch'));	
 });
 
 
 
-gulp.task('dev', gulp.series('buildCss','buildHtml','buildJs','browser-sync'));
+gulp.task('dev', gulp.series('buildCss','buildHtml', 'buildImgs','buildJs','browser-sync'));
 // gulp.task('prod', ['buildCss','buildJs','buildImgs','buildFonts','buildHtml']);
